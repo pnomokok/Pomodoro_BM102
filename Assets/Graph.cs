551fae915a55afc47877256d0af7f8f104f1c07c
aeriaIz <su.yildiz@msn.com>
2024-05-17 14:55:53 +0300

    AAA

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -7,11 +7,20 @@ public class Graph : MonoBehaviour
 {
     [SerializeField] private Sprite circleSprite;
     private RectTransform GraphContainer;
-    private void Awake()
+    public SaveLessonData saveLessonData;
+
+    void Start()
     {
-        GraphContainer = transform.Find("GraphContainer").GetComponent<RectTransform>();
-        CreateCircle(new Vector2(200, 200));
+        if (saveLessonData != null)
+        {
+            saveLessonData.confirmButton.onClick.AddListener(CreateGraph);
+        }
+        else
+        {
+            Debug.Log("SaveLessonData scripti atanmamýþ veya eksik!");
+        }
     }
+
     private void CreateCircle(Vector2 anchoredPosition)
     {
         GameObject gameObject = new GameObject("circle", typeof(Image));
@@ -23,4 +32,45 @@ public class Graph : MonoBehaviour
         rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(0, 0);
     }
+
+    void CreateGraph()
+    {
+        if (saveLessonData != null)
+        {
+            CreateCircle(new Vector2(200, saveLessonData.toplamNet));
+        }
+        else
+        {
+            Debug.LogWarning("SaveLessonData scripti atanmamýþ veya eksik!");
+        }
+    }
 }
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class Graph : MonoBehaviour
+//{
+//    [SerializeField] private Sprite circleSprite;
+//    private RectTransform GraphContainer;
+//    public SaveLessonData saveLessonData;
+
+//    private void Awake()
+//    {
+//        GraphContainer = transform.Find("GraphContainer").GetComponent<RectTransform>();
+//        CreateCircle(new Vector2(200, saveLessonData.toplamNet));
+//    }
+
+//    private void CreateCircle(Vector2 anchoredPosition)
+//    {
+//        GameObject gameObject = new GameObject("circle", typeof(Image));
+//        gameObject.transform.SetParent(GraphContainer, false);
+//        gameObject.GetComponent<Image>().sprite = circleSprite;
+//        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+//        rectTransform.anchoredPosition = anchoredPosition;
+//        rectTransform.sizeDelta = new Vector2(11, 11);
+//        rectTransform.anchorMin = new Vector2(0, 0);
+//        rectTransform.anchorMax = new Vector2(0, 0);
+//    }
+//}
--- Assets/SaveLessonData.cs
+++ Assets/SaveLessonData.cs
@@ -20,7 +20,22 @@ public class SaveLessonData : MonoBehaviour
     public TMP_InputField SosyalWrongInputField;
     public TMP_InputField SosyalEmptyInputField;
 
+    public float toplamNet;
+
     public LessonData lessonData;
+
+    public Button confirmButton;
+
+    void Start()
+    {
+        confirmButton.onClick.AddListener(ConfirmButtonClicked);
+    }
+
+    void ConfirmButtonClicked()
+    {
+        SaveData();
+        CalculateTotalNet();
+    }
 
     public void SaveData()
     {
@@ -40,4 +55,55 @@ public class SaveLessonData : MonoBehaviour
         lessonData.SosyalWrongAnswers = int.Parse(SosyalWrongInputField.text);
         lessonData.SosyalEmptyAnswers = int.Parse(SosyalEmptyInputField.text);
     }
+    public void CalculateTotalNet()
+    {
+        toplamNet = (lessonData.TurkceCorrectAnswers + lessonData.MatematikCorrectAnswers + lessonData.FenCorrectAnswers + lessonData.SosyalCorrectAnswers) - (lessonData.TurkceWrongAnswers + lessonData.MatematikWrongAnswers + lessonData.FenWrongAnswers + lessonData.SosyalWrongAnswers) / 4.0f;
+    }
 }
+//using UnityEngine;
+//using UnityEngine.UI;
+//using TMPro;
+
+//public class SaveLessonData : MonoBehaviour
+//{
+//    public TMP_InputField TurkceCorrectInputField;
+//    public TMP_InputField TurkceWrongInputField;
+//    public TMP_InputField TurkceEmptyInputField;
+
+//    public TMP_InputField MatematikCorrectInputField;
+//    public TMP_InputField MatematikWrongInputField;
+//    public TMP_InputField MatematikEmptyInputField;
+
+//    public TMP_InputField FenCorrectInputField;
+//    public TMP_InputField FenWrongInputField;
+//    public TMP_InputField FenEmptyInputField;
+
+//    public TMP_InputField SosyalCorrectInputField;
+//    public TMP_InputField SosyalWrongInputField;
+//    public TMP_InputField SosyalEmptyInputField;
+
+//    public float toplamNet;
+
+//    public LessonData lessonData;
+
+//    public void SaveData()
+//    {
+//        lessonData.TurkceCorrectAnswers = int.Parse(TurkceCorrectInputField.text);
+//        lessonData.TurkceWrongAnswers = int.Parse(TurkceWrongInputField.text);
+//        lessonData.TurkceEmptyAnswers = int.Parse(TurkceEmptyInputField.text);
+
+//        lessonData.MatematikCorrectAnswers = int.Parse(MatematikCorrectInputField.text);
+//        lessonData.MatematikWrongAnswers = int.Parse(MatematikWrongInputField.text);
+//        lessonData.MatematikEmptyAnswers = int.Parse(MatematikEmptyInputField.text);
+
+//        lessonData.FenCorrectAnswers = int.Parse(FenCorrectInputField.text);
+//        lessonData.FenWrongAnswers = int.Parse(FenWrongInputField.text);
+//        lessonData.FenEmptyAnswers = int.Parse(FenEmptyInputField.text);
+
+//        lessonData.SosyalCorrectAnswers = int.Parse(SosyalCorrectInputField.text);
+//        lessonData.SosyalWrongAnswers = int.Parse(SosyalWrongInputField.text);
+//        lessonData.SosyalEmptyAnswers = int.Parse(SosyalEmptyInputField.text);
+
+//        toplamNet = (lessonData.TurkceCorrectAnswers + lessonData.MatematikCorrectAnswers + lessonData.FenCorrectAnswers + lessonData.SosyalCorrectAnswers) - (lessonData.TurkceWrongAnswers + lessonData.MatematikWrongAnswers + lessonData.FenWrongAnswers + lessonData.SosyalWrongAnswers) / 4.0f;
+//    }
+//}
